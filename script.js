90351f40d62302454d350bc84bc41eb26c444803
Code_Caster <53581330+Necr0Coder@users.noreply.github.com>
2022-08-19 13:39:38 -0400

    corrected functions

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-add(a, b) {
-    a + b
-}
-
-subtract(a, b) {
-    a - b
-}
-
-multiply (a, b){
-    a * b
-}
-
-divide (a, b) {
-    a / b
-}
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
