da2717738fa16f4003a77e1b74dc2b91bbbc77bf
necr0coder <jrhurley5333@gmail.com>
2022-09-05 21:22:18 -0400

    added buttons. basic css

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const add = (a, b) => a + b;
+console.log(add(5, 5));
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+const operate = (operator, numA, numB) => {
+  return operator(numA, numB);
+};
