f312d09c8280fd88f635d6332de9ea64f9f0e1fd
Josh H <jrhurley5333@gmail.com>
2022-09-07 21:17:48 -0400

    added eventListeners to nums. Display shows currentValue and operators

--- script.js
+++ script.js
@@ -6,3 +6,59 @@ const divide = (a, b) => a / b;
 const operate = (operator, numA, numB) => {
   return operator(numA, numB);
 };
+let operator = "";
+let currentValue = "";
+let previousValue = "";
+
+//!. add event listners to buttons.   DOMContentLoaded = loads js after HTML but doesn't wait for stylesheets.
+
+document.addEventListener("DOMContentLoaded", function () {
+  let clear = document.querySelector(".clear");
+  let negPos = document.querySelector(".negPos");
+  let decimal = document.querySelector(".decimal");
+  let equal = document.querySelector("#equals");
+  let display = document.querySelector(".display");
+
+  let numbers = document.querySelectorAll(".operand");
+  let operators = document.querySelectorAll(".operator");
+
+  numbers.forEach((number) => {
+    number.addEventListener("click", (e) => {
+      currentValue = "";
+      displayNumber(e.target.textContent);
+      display.textContent = currentValue;
+    });
+  });
+
+  operators.forEach((op) => {
+    op.addEventListener("click", (e) => {
+      displayOperator(e.target.textContent);
+      display.textContent = previousValue + " " + operator;
+    });
+  });
+
+  clear.addEventListener("click", (e) => {
+    display.textContent = "";
+    currentValue = "";
+  });
+});
+
+//2. make numbers show on display
+
+function displayNumber(num) {
+  if (currentValue.length <= 9) {
+    currentValue += num;
+  }
+}
+
+function displayOperator(op) {
+  operator = op;
+  previousValue = currentValue;
+}
+
+//3. make numbers then operators on display
+//4. make computation work
+//5. limit numbers on display
+//6. clear display
+//7. continuous computation
+//8. finished?
