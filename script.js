8e25ac74bf50784397e63788e8667341c0e8512a
necr0coder <jrhurley5333@gmail.com>
2022-09-12 22:02:55 -0400

    basic computation works.

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const add = (a, b) => a + b;
-console.log(add(5, 5));
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
-const operate = (operator, numA, numB) => {
-  return operator(numA, numB);
-};
+
 let operator = "";
 let currentValue = "";
 let previousValue = "";
+let newValue = "";
 
-//!. add event listners to buttons.   DOMContentLoaded = loads js after HTML but doesn't wait for stylesheets.
+//add event listners to buttons.   DOMContentLoaded = loads js after HTML but doesn't wait for stylesheets.
 
 document.addEventListener("DOMContentLoaded", function () {
   let clear = document.querySelector(".clear");
@@ -24,7 +22,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   numbers.forEach((number) => {
     number.addEventListener("click", (e) => {
-      currentValue = "";
       displayNumber(e.target.textContent);
       display.textContent = currentValue;
     });
@@ -37,28 +34,75 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+  equal.addEventListener("click", (e) => {
+    operate(operator, previousValue, currentValue);
+    display.textContent = newValue;
+    if ((newValue = 8008135)) {
+      display.textContent = "( @ Y @ )";
+    }
+  });
+
   clear.addEventListener("click", (e) => {
     display.textContent = "";
     currentValue = "";
+    previousValue = "";
+    newValue = "";
+    operator = "";
   });
 });
 
-//2. make numbers show on display
+//make numbers show on display.   limit numbers on display
 
 function displayNumber(num) {
   if (currentValue.length <= 9) {
     currentValue += num;
   }
+  console.log("prev num is " + previousValue);
+  console.log("current num is " + currentValue);
 }
 
+//make numbers then operators on display
 function displayOperator(op) {
   operator = op;
   previousValue = currentValue;
+  currentValue = "";
+  console.log("operator is " + operator);
 }
 
-//3. make numbers then operators on display
-//4. make computation work
-//5. limit numbers on display
-//6. clear display
-//7. continuous computation
-//8. finished?
+//make computation work
+function operate(op, a, b) {
+  op = operator;
+  a = Number(previousValue);
+  b = Number(currentValue);
+
+  console.log("previous value is " + a);
+  console.log("current value is " + b);
+
+  if ((a == "") & (b == "")) {
+    newValue = "try again";
+    return newValue;
+  }
+  if (op == "+") {
+    newValue = add(a, b);
+    return newValue;
+  }
+  if (op == "-") {
+    newValue = subtract(a, b);
+    return newValue;
+  }
+  if (op == "/") {
+    newValue = divide(a, b);
+    return newValue;
+  }
+  if (op == "*") {
+    newValue = multiply(a, b);
+    return newValue;
+  }
+  previousValue = newValue;
+}
+
+//continuous computation
+
+//decimals
+
+//negative positive
