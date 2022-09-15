7e029deb879ccee4f89620edd03cf549e037f7ce
Josh H <jrhurley5333@gmail.com>
2022-09-14 22:16:59 -0400

    trying to add new displays

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-let operator = "";
 let currentValue = "";
 let previousValue = "";
+let operator = "";
 let newValue = "";
 
 //add event listners to buttons.   DOMContentLoaded = loads js after HTML but doesn't wait for stylesheets.
@@ -15,7 +15,8 @@ document.addEventListener("DOMContentLoaded", function () {
   let negPos = document.querySelector(".negPos");
   let decimal = document.querySelector(".decimal");
   let equal = document.querySelector("#equals");
-  let display = document.querySelector(".display");
+  let mainDisplay = document.querySelector(".main");
+  let miniDisplay = document.querySelector(".mini");
 
   let numbers = document.querySelectorAll(".operand");
   let operators = document.querySelectorAll(".operator");
@@ -23,24 +24,26 @@ document.addEventListener("DOMContentLoaded", function () {
   numbers.forEach((number) => {
     number.addEventListener("click", (e) => {
       displayNumber(e.target.textContent);
-      display.textContent = currentValue;
+      mainDisplay.textContent = currentValue;
+      miniDisplay.textContent = previousValue + "" + operator;
     });
   });
 
   operators.forEach((op) => {
     op.addEventListener("click", (e) => {
       displayOperator(e.target.textContent);
-      display.textContent = previousValue + " " + operator;
+      mainDisplay.textContent = previousValue + " " + operator;
     });
   });
 
   equal.addEventListener("click", (e) => {
     operate(operator, previousValue, currentValue);
-    display.textContent = newValue;
+    mainDisplay.textContent = newValue;
   });
 
   clear.addEventListener("click", (e) => {
-    display.textContent = "";
+    mainDisplay.textContent = "";
+    miniDisplay.textContent = "";
     currentValue = "";
     previousValue = "";
     newValue = "";
@@ -51,11 +54,14 @@ document.addEventListener("DOMContentLoaded", function () {
 //make numbers show on display.   limit numbers on display
 
 function displayNumber(num) {
+  //removes previous number
+  if (operator == true) {
+    currentValue = "";
+  }
+
   if (currentValue.length <= 9) {
     currentValue += num;
   }
-  console.log("prev num is " + previousValue);
-  console.log("current num is " + currentValue);
 }
 
 //make numbers then operators on display
@@ -63,39 +69,31 @@ function displayOperator(op) {
   operator = op;
   previousValue = currentValue;
   currentValue = "";
-  console.log("operator is " + operator);
 }
 
 //make computation work
 function operate(op, a, b) {
   op = operator;
-  a = Number(previousValue);
-  b = Number(currentValue);
-
-  console.log("previous value is " + a);
-  console.log("current value is " + b);
+  a = Number(a);
+  b = Number(b);
 
   if ((a == "") & (b == "")) {
     newValue = "try again";
-    return newValue;
   }
   if (op == "+") {
     newValue = add(a, b);
-    return newValue;
   }
   if (op == "-") {
     newValue = subtract(a, b);
-    return newValue;
   }
   if (op == "/") {
     newValue = divide(a, b);
-    return newValue;
   }
   if (op == "*") {
     newValue = multiply(a, b);
-    return newValue;
   }
-  previousValue = newValue;
+
+  currentValue = newValue;
 }
 
 //continuous computation
